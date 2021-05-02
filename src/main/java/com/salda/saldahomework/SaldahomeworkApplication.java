d073babfccc9d14c8bb1e641a44cee8ceeaccd23
hagyuho <hagyuho90@gmail.com>
2021-05-03 00:01:26 +0900

    salda toy project

---
+++ src/main/java/com/salda/saldahomework/SaldahomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.salda.saldahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaldahomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaldahomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salda/saldahomework/SaldahomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salda.saldahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaldahomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
